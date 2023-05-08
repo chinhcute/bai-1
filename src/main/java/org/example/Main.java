5a45e1d79c6e240dbe9da182412c8f452eaa8758
ADMIN <chinh20043008@gmail.com>
2023-05-08 20:20:35 +0700

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        accout accout1 = (accout) context.getBean("account1");
+        accout accout2 = (accout) context.getBean("account2");
+        System.out.println("accout1");
+        System.out.println(accout1.getId());
+        System.out.println(accout1.getName());
+        System.out.println(accout1.getLuong());
+        System.out.println("accout2");
+        System.out.println(accout2.getId());
+        System.out.println(accout2.getName());
+        System.out.println(accout2.getLuong());
+
+    }
+}
---
+++ src/main/java/org/example/accout.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class accout {
+    private long id;
+    private String name;
+    private  double luong;
+
+    public accout() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLuong() {
+        return luong;
+    }
+
+    public void setLuong(double luong) {
+        this.luong = luong;
+    }
+}
